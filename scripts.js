05b34d72a00cbc45883f098b23783e75f27b1b0c
angusperkins <perkinsangus@gmail.com>
2024-06-18 12:06:21 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
